cd7b8c7c4b047b91b108730d05cd2a5c4027b2c7
Marcos Caceres <marcosscaceres@gmail.com>
2012-09-06 01:15:50 +0100

    Added a todo about needing to patch Intl.DateTimeFormat

--- jsi18n_patch.js
+++ jsi18n_patch.js
@@ -37,6 +37,11 @@
 * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 * OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* TODO: need to patch Intl.DateTimeFormat to support strings as the first
+*       argument. Currently, only accepts an array.
+*		
+*
 **/
 (function(global) {
     'use strict';
